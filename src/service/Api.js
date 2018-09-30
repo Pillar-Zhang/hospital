e7c5ab185dad3e3ceaabcaf1856d7b3b841004ea
mac-xiaodong <hnhbxiaodong@163.com>
2018-09-30 12:23:01 +0800

    容错性别

--- src/service/Api.js
+++ src/service/Api.js
@@ -2,7 +2,7 @@
 const baseUrl = "http://darrenluo.vaiwan.com:8081";
 export const PostMedical = data => {
   // delete data.photos;
- data.sex = Array.isArray(data.sex)?data.sex.join()||"男"
+  data.sex = Array.isArray(data.sex) ? data.sex.join() : "男";
   console.log(data, "get");
   return fetch(`${baseUrl}/medicals`, {
     method: "POST",
@@ -23,7 +23,7 @@ export const GetMedicalById = id => {
 };
 
 export const PutMedicalById = data => {
-  data.sex = data.sex.join();
+  data.sex = Array.isArray(data.sex) ? data.sex.join() : "男";
   console.log(data, "put");
   return fetch(`${baseUrl}/medicals`, {
     method: "PUT",
